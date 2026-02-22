df9ceb13c34bc55cf0fba5a86c4bd282549c159e
Valentin <valentin15061972@gmail.com>
2026-02-22 16:49:30 +0300

    Update models-bot.py

--- models-bot.py
+++ models-bot.py
@@ -1,8 +1,17 @@
+import sqlalchemy
+import os
+import json
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 from sqlalchemy import Column, String, Integer, ForeignKey, BigInteger
 from sqlalchemy.orm import relationship, declarative_base
 
 
 Base = declarative_base()
+load_dotenv()
+DSN = os.getenv('DSN')
+engine = sqlalchemy.create_engine(DSN)
+Session = sessionmaker(bind=engine)
 
 
 class Word(Base):
@@ -10,7 +19,8 @@ class Word(Base):
     id = Column(Integer, primary_key=True)
     russian = Column(String, nullable=False)
     english = Column(String, nullable=False)
-
+    def __str__(self):
+        return f"{self.english} - {self.russian}"
 
 class User(Base):
     __tablename__ = 'users'
@@ -26,3 +36,15 @@ class UserWord(Base):
 
     user = relationship("User", backref="user_words")
     word = relationship("Word", backref="user_words")
+
+
+def load_db():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    with Session() as session:
+        if session.query(Word).count() == 0:
+            with open('data_bot.json', 'r', encoding='utf-8') as f:
+                words = json.load(f)
+            for w in words:
+                session.add(Word(russian=w['russian'], english=w['english']))
+            session.commit()
